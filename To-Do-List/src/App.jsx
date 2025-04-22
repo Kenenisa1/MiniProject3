610fa010f1c9b705fcea349eda5ec384a9f42462
KENENISA MB <kenenisamiesob@Gmail.com>
2025-04-22 08:00:33 +0000

    ToDoList compnonent added to App

--- To-Do-List/src/App.jsx
+++ To-Do-List/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import viteLogo from '/vite.svg'
+import ToDoList from './ToDoList'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-
+        <ToDoList/>
     </>
   )
 }
